5d06d730c61f143d74b9b214d84226a5461dac12
Ton Kuijt <github@kuijt.nu>
2019-01-15 16:11:09 +0100

    Code commented for readability

--- Reaction.ino
+++ Reaction.ino
@@ -1,59 +1,36 @@
-/* Knock Sensor
-
- * ----------------
-
+/* Reaction Timer
+ * 
+ * Testing the levels that the Piezo sees when shooting the plate.
+ * Using the basic setup (Piezo minus to gnd, plus to A(nalog)0, 680kOhm resistor parallel
+ * the thresholds are as displayed.
  *
-
- * Program using a Piezo element as if it was a knock sensor.
-
+ * The program does as follows:
  *
-
- * We have to basically listen to an analog pin and detect 
-
- * if the signal goes over a certain threshold. It writes
-
- * "knock" to the serial port if the Threshold is crossed,
-
- * and toggles the LED on pin 13.
-
- *
-
- * (cleft) 2005 D. Cuartielles for K3
-
- * edited by Scott Fitzgerald 14 April 2013
-
+ * It waits 10 seconds blinking the LED slow.. Then, it blinks 3 times to indicate it's going soon. 
+ * Then it waits randomly between 2 and 10 seconds and lights the LED. This is the shoot signal, when the LED lights 
+ * you have to shoot the plate as fast as possible.
+ * When the plate is shot, the LED extinguishes and the serial monitor then show your reaction time. The 10 second load timer
+ * starts again, and the whole sequence repeats itself.
  */
 
 
 
 int ledPin = 13;
-
 int knockSensor = 0;               
-
 byte val = 0;
-
 int statePin = LOW;
-
 int THRESHOLD = 20;
-
-
-
-void setup() {
-
- pinMode(ledPin, OUTPUT);
- pinMode(4, OUTPUT);
- digitalWrite(4, LOW); 
-
- Serial.begin(9600);
- Serial.println("Program started");
-
-}
-
-
 int count = 0;
 
-void loop() {
-  
+void setup() {
+ pinMode(ledPin, OUTPUT);
+ pinMode(4, OUTPUT);
+ digitalWrite(4, LOW);
+ Serial.begin(9600);
+ Serial.println("Program started");
+}
+
+void loop() { 
 // Wait 10 secs for reload
 // Slow LED for reload
 Serial.println("10 seconds for reload...");
@@ -77,6 +54,7 @@ while (blin < 10) {
   blin +=1;
 }
 
+ // Wat randomly between 2 and 10 seonds
   int waittime=random(2000, 10000);
   Serial.print("Waiting for ");
   Serial.print(waittime);
@@ -85,24 +63,22 @@ while (blin < 10) {
 
 // OK, get time from somewhere and store it. Then turn LED HIGH and wait for knock...
 
-int timer = millis();
-digitalWrite(4, HIGH);
-
+  int timer = millis();
+  digitalWrite(4, HIGH);
   val = analogRead(knockSensor);  
-
   while (val <= THRESHOLD) {
-    delay(10);
-    val = analogRead(knockSensor);
-  }
+      delay(10);
+      val = analogRead(knockSensor);
+    }
   digitalWrite(4, LOW);
 
-int outtime = millis();
-int
-reaction = outtime - timer;
-Serial.print("Reacted in ");
-Serial.print(reaction);
-Serial.println(" ms");
-
-delay(2000);
+  int outtime = millis();
+  int reaction = outtime - timer;
+  Serial.print("Reacted in ");
+  Serial.print(reaction);
+  Serial.println(" ms");
+ 
+ // Done, sleep 5 secs then start over
+ delay(5000);
 
 }
